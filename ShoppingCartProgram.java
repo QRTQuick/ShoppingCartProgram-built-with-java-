c11e16b44572d8a1d5296a8c160e54e0f7a0160e
chisom life eke <chisomlifeeke@gmail.com>
2025-10-22 21:26:09 +0100

    Initial commit

--- ShoppingCartProgram.java
+++ ShoppingCartProgram.java
@@ -1,27 +1,36 @@
-// import scanner 
 import java.util.Scanner;
-public class ShoppingCartProgram{
-    public static void main(String[] argrs){
-	    // Shopping Cart Program project 2025 exersise bro code
-		Scanner scanner = new Scanner(System.in);
-		//what customer what to buy 
-		System.out.print("what item do u wana buy : ");
-		String item = scanner.nextLine();
-		// price for item
-		System.out.print("Whats the price on the price tag : ");
-		double PriceOfItem = scanner.nextDouble();
-		// no of items user wants to buy 
-		System.out.print("How many are you taking : ");
-		long NumberOfItem = scanner.nextLong();
-		// calculation or price engine 
-		double FinalCost;
-		FinalCost = PriceOfItem*NumberOfItem;
-		
-		// engine output
-        System.out.print("you have purchased "+NumberOfItem+" "+item+"/s");
-        System.out.print("your total cost is N"+FinalCost);		
-		
-		
-		scanner.close();	 
-	  }
+
+public class ShoppingCartProgram {
+    public static void main(String[] args) {
+        // Shopping Cart Program - Bro Code 2025 Exercise
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask what customer wants to buy
+        System.out.print("What item do you want to buy: ");
+        String item = scanner.nextLine();
+
+        // Ask for price of the item
+        System.out.print("What's the price on the price tag: ");
+        double priceOfItem = scanner.nextDouble();
+
+        // Ask how many they want to buy
+        System.out.print("How many are you taking: ");
+        long numberOfItems = scanner.nextLong();
+
+        // Logic and output
+        if (numberOfItems > 1) {
+            double finalCost = priceOfItem * numberOfItems;
+            System.out.println("You have purchased " + numberOfItems + " " + item + "(s).");
+            System.out.println("Your total cost is ₦" + finalCost);
+        } 
+        else if (numberOfItems == 1) {
+            System.out.println("You have purchased 1 " + item + ".");
+            System.out.println("Your total cost is ₦" + priceOfItem);
+        } 
+        else {
+            System.out.println("You have not made any purchases yet. Only product name = "+ item + " and its price = ₦" + priceOfItem);
+        }
+
+        scanner.close();
+    }
 }
